17420513ad34549e1d06267bd31c1fbbc33a5bfb
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2025-01-23 21:52:33 +0530

    Setup basic api structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import serverConfig from './config/serverConfig';
+import apiRouter from './routes';
 
 
 const app = express();
+app.use('/api', apiRouter)
 app.listen(serverConfig.PORT, ()=>{
     console.log(`Server started on port ${serverConfig.PORT}`);
     console.log('Press CTRL + C to stop the server');

a72bb8effe3db12c9d17fa7cb8abf4cc57c24818
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2025-01-27 21:00:18 +0530

    Integrated BullBoard

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import serverConfig from './config/serverConfig';
 import apiRouter from './routes';
 import sampleQueueProducer from './producers/sampleQueueProducer';
 import SampleWorker from './workers/SampleWorker';
+import serverAdapter from './config/bullBoardConfig';
 
 
 const app = express();
 app.use('/api', apiRouter)
+app.use('/ui', serverAdapter.getRouter())
 app.listen(serverConfig.PORT, ()=>{
     console.log(`Server started on port ${serverConfig.PORT}`);
     SampleWorker('SampleQueue')

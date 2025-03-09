b156b82d1a239d0e651271bacf379c6d27f2e1fa
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2025-03-09 15:01:28 +0530

    Added execution code for CPP and JAVA

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
-export  const  PYTHON_IMAGE = "python:3.8-slim"
-
+export const PYTHON_IMAGE = "python:3.8-slim"
+export const JAVA_IMAGE = "openjdk:11-jdk-slim"
+export const CPP_IMAGE = "gcc:latest" 
 //This will represent the header size of the docker stream
 //docker stream header will contain data about the type of stream i.e., stdout/stderr
 // and length of the data

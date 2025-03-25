0ceb66ce5f6ec1ce04c2b30fe41e31f047ea28e0
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2025-03-25 22:23:45 +0530

    Added Generic code creator

---
+++ src/utils/codeCreate.ts
@@ -0,0 +1,9 @@
+export default CodeCreator (startingCode: string, middleCode: string, endCode: string) : string {
+    return `
+        ${startingCode}
+
+        ${middleCode}
+
+        ${endCO}
+    `
+}

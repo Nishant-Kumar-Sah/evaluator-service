080202370a4a23db0254742c20865127ce850294
Nishant-Kumar-Sah <nishant.sah2001@gmail.com>
2025-03-25 22:24:39 +0530

    minor corrections

--- src/utils/codeCreate.ts
+++ src/utils/codeCreate.ts
@@ -1,9 +1,9 @@
-export default CodeCreator (startingCode: string, middleCode: string, endCode: string) : string {
+export default function CodeCreator (startingCode: string, middleCode: string, endCode: string) : string {
     return `
         ${startingCode}
 
         ${middleCode}
 
-        ${endCO}
+        ${endCode}
     `
 }
